d8d310570a12072269d187919f8a56f29ec40f34
Zitrusherz <ch.euribe@duocuc.cl>
2024-01-24 19:58:39 -0300

    pre alpha 1.01

---
+++ MusicRecord2/src/musicRecord/MusicRecord.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package musicRecord;
+
+/**
+ *
+ * @author chris
+ */
+public class MusicRecord{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        
+        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                System.out.println(info.getName());
+                    
+        }
+        
+        java.awt.EventQueue.invokeLater(() -> {
+            new MusicRecordFrame().setVisible(true);
+        });
+    }
+}
+    
+
